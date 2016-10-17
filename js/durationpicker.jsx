32b7fb72112b5de0ceb451aaa32398b2c33f1274
Ian Moore <ian.mark.moore@gmail.com>
2016-10-16 20:42:26 -0500

    style duration picker

--- js/durationpicker.jsx
+++ js/durationpicker.jsx
@@ -4,12 +4,12 @@ class DurationPicker extends React.Component {
     render() {
         return (
             <div className="duration-picker">
-                <label htmlFor="duration-short">Short Term</label>
                 <input type="radio" id="duration-short" name="duration" value="short_term" onChange={this.props.onChange} />
-                <label htmlFor="duration-medium">Medium Term</label>
+                <label htmlFor="duration-short">Short Term</label>
                 <input type="radio" id="duration-medium" name="duration" value="medium_term" onChange={this.props.onChange} defaultChecked />
-                <label htmlFor="duration-long">Long Term</label>
+                <label htmlFor="duration-medium">Medium Term</label>
                 <input type="radio" id="duration-long" name="duration" value="long_term" onChange={this.props.onChange} />
+                <label htmlFor="duration-long">Long Term</label>
             </div>
         )
     }
